9a8ce71456d0049299c71a770c32d495f83dff91
Maria232627 <maria.lscarvalho01@gmail.com>
2023-06-02 08:17:02 -0300

    Segunda Atualização

--- script.js
+++ script.js
@@ -2,14 +2,60 @@ const listElement = document.querySelector("#app ul")
 const inputElement = document.querySelector("#app input")
 const buttonElement = document.querySelector("#app button")
 
-const tarefas = ["Estudar para a prova", "Dormir cedo"]
+const tarefas = JSON.parse(localStorage.getItem("list")) || []
+//["Estudar para a prova", "Dormir cedo"]
 
-for (const iterator of tarefas) {
+function renderTarefas() {
+    listElement.innerHTML = ""
+    for (const iterator of tarefas) {
+        const tarefaElement = document.createElement("li")
+        const tarefaText = document.createTextNode(iterator)
+
+        const linkElement = document.createElement("a")
+        linkElement.setAttribute("href", "#")
+
+        const pos = tarefas.indexOf(iterator)
+        linkElement.setAttribute("onclick", `deleteTarefa(${pos})`)
+
+        const linkText = document.createTextNode("Excluir")
+        linkElement.appendChild(linkText)
+
+        tarefaElement.appendChild(tarefaText)
+        tarefaElement.appendChild(linkElement)
+        listElement.appendChild(tarefaElement)
+        //console.log(iterator)
+    }
+    //console.log(listElement,inputElement,buttonElement)
+}
+renderTarefas()
+
+/*for(let i = 0; i<tarefas.length; i++){
     const tarefaElement = document.createElement("li")
     const tarefaText = document.createTextNode(iterator)
+    
 
     tarefaElement.appendChild(tarefaText)
-    listElement.appendChild(tarefaElement)
-    //console.log(iterator)
+    listElement.appendChild(tarefaElement) 
 }
-//console.log(listElement,inputElement,buttonElement)
+*/
+
+
+function addTarefas() {
+    const tarefaText = inputElement.value
+    console.log(tarefaText)
+    tarefas.push(tarefaText)
+    inputElement.value = ""
+    renderTarefas()
+    saveToStorage()
+}
+buttonElement.onclick = addTarefas
+
+function deleteTarefa(pos) {
+    tarefas.splice(pos, 1)
+    renderTarefas()
+    saveToStorage()
+}
+
+function saveToStorage() {
+    localStorage.setItem("list", JSON.stringify(tarefas))
+}
